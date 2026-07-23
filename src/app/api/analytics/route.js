fee1c46483b2e3561abca31968eea7e89e7986a5
Naeem Ud Din <uddinn874@gmail.com>
2026-07-24 02:22:07 +0500

    fix: Dynamic server usage in Analytics API

--- src/app/api/analytics/route.js
+++ src/app/api/analytics/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../auth/[...nextauth]/authOptions.js';
 import dbConnect from '../../../lib/dbConnect.js';
